d307bae2b739cf74755873d57dda651b22232a49
streammachineio-org <apis@streammachine.io>
2021-08-06 08:55:17 +0000

    chore(release): 1.0.0
    # 1.0.0 (2021-08-06)
    
    ### Features
    
    * common 1.0.0 ([2ea3dee](https://github.com/streammachineio/python-driver/commit/2ea3deec45cc1d7802e81fecea0910c0c6688f3a))
    * force 1.0.0 release ([3f16564](https://github.com/streammachineio/python-driver/commit/3f1656490a68c3ce859c57b187c993318ef647ce))
    * updated to get_strm_schema_ref ([e8024ef](https://github.com/streammachineio/python-driver/commit/e8024ef7ee65aef455c5b7dfe0d804066368c1bc))

--- streammachine/driver/__init__.py
+++ streammachine/driver/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Stream Machine B.V."""
 __email__ = 'apis@streammachine.io'
-__version__ = '0.9.9'
+__version__ = '1.0.0'
 
 from .client import StreamMachineClient
 from streammachine.schemas.common import StreamMachineEvent
